a8784243724fe433423630bc206e160c48629dd1
Arif <arif.k-34@outlook.com>
2024-10-28 13:03:50 +0300

    Initial Commit

--- api/save_code.js
+++ api/save_code.js
@@ -1,9 +1,8 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        const { code } = req.body; // Get the authorization code from the request body
+        const { code } = req.body;
         console.log('Received authorization code:', code);
-        // You can process the code here (e.g., exchange it for an access token)
-        res.status(200).json({ message: 'Code saved successfully!' }); // Send a success response
+        res.status(200).json({ message: 'Code received successfully!' });
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
